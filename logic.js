229d8278a93457e2824f5439beb73300fbf369ec
jsubbie <subbie.jason@gmail.com>
2018-03-23 14:25:11 -0400

    updated data

--- logic.js
+++ logic.js
@@ -1,28 +1,102 @@
+// Mapbox API
+
+var mapbox = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ.T6YbdDixkOBWH_k9GbS8JQ";
+
 // Creating map object
-var map = L.map("map", {
-  zoom: 11, 
-  continuousWorld: false,
-  noWrap: true
+var myMap = L.map("map", {
+  center: [40.7, -73.95],
+  zoom: 3
 });
 
-map.fitWorld().zoomIn();
+// Adding tile layer to the map
+L.tileLayer(mapbox).addTo(myMap);
 
-map.setMaxBounds(map.getBounds());
+// Building API query URL
+
+//var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
+//var date = "$where=created_date between'2016-01-10T12:00:00' and '2017-01-01T14:00:00'";
+//var complaint = "&complaint_type=Rodent";
+//var limit = "&$limit=10000";
+// Assembling API query URL
+/*
+var baseURL = {
+        "Abilities": "['Insomnia', 'Forewarn', 'Inner Focus']",
+        "AppearedDay": "2",
+        "AppearedDayOfWeek": "Tuesday",
+        "AppearedLocalTime": "9/2/16 9:50:06 PM",
+        "AppearedMonth": "8",
+        "AppearedTimeOfDay": "night",
+        "AppearedYear": "2016",
+        "City": "Prague",
+        "Class": "96",
+        "Classfication": "Hypnosis Pokémon",
+        "CloseToWater": "False",
+        "Continent": "Europe",
+        "Generation": "1",
+        "IsLegendary": "0",
+        "JapaneseName": "Sleepeスリープ",
+        "Latitude": "50.144272",
+        "Longitude": "14.101178",
+        "Name": "Drowzee",
+        "NumberofRecords": "1",
+        "PercentageMale": "50",
+        "PokedexNumber": "96",
+        "PokemonId": "96",
+        "SpAttack": "43",
+        "SpDefense": "90",
+        "Speed": "42",
+        "TerrainType": "12",
+        "Type1": "psychic",
+        "Type2": "",
+        "Weather": "Clear",
+        "WeightKg": "32.4",
+        "_Id": "MTMzNzM3NzQ1MTM4MTcwMTE0Njk="
+    };
+*/
+
+  var jsonURL = "http://0.0.0.0:8080/Data/PokemonData.json";
+
+  d3.json(jsonURL, function(error, response) {
+  console.log("test");
+  console.log(error);
+  console.log(response);
 
 
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ.T6YbdDixkOBWH_k9GbS8JQ").addTo(map);
+  // Creating a new marker cluster group
 
-  L.tileLayer.provider('Stamen.Watercolor').addTo(map);  
+     var markers = L.markerClusterGroup();
 
-var link = "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/" +
-"35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
+  // Loop through our data...
+    for (var i = 0; i < response.length; i++) {
+    // set the data location property to a variable
+      var Lat = response[i].Latitude;
+      var Lon = response[i].Longitude;
+
+    // If the data has a location property...
+      if (Lat) {
 
 
+    var pokemonIcon = L.icon({
+        iconUrl:  response[i].ImageURL,
+        iconSize:     [100, 100], // size of the icon
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
 
-// Grabbing our GeoJSON data..
-d3.json(link, function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(map);
+    if (response[i].ImageURL) { 
+      markers.addLayer(L.marker([Lat, Lon], {icon: pokemonIcon})
+      .bindPopup("Name : " + response[i].Name + " Type : " + response[i].Type2  + "<br>Weather : <br>" + response[i].Weather + pokemonIcon));
+    }
+
+    else {
+      markers.addLayer(L.marker([Lat, Lon])
+      .bindPopup("Name : " + response[i].Name + " Type : " + response[i].Type2  + "<br>Weather : <br>" + response[i].Weather));      
+    }
+
+     }
+   }
+    myMap.addLayer(markers);
 });
+
+
+
